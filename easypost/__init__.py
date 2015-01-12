09c0741933a784149275410d34ad8ea0d3026fa0
Gabriel Sinkin <gabriel.sinkin@gmail.com>
2015-01-12 11:09:42 -0800

    I have no idea what verified wasnt set

--- easypost/__init__.py
+++ easypost/__init__.py
@@ -286,7 +286,7 @@ class Requestor(object):
                         "Please report to contact@easypost.com." % method)
 
         try:
-            result = requests.request(method, abs_url, headers=headers, data=data, timeout=60, verify=False)
+            result = requests.request(method, abs_url, headers=headers, data=data, timeout=60)
             http_body = result.text
             http_status = result.status_code
         except Exception as e:
