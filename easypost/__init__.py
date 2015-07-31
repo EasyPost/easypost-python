c8d482d13a1c7e6e2429723d1825b3d66faf6a12
Jake Epstein <jake@easypost.com>
2015-07-31 13:17:42 -0700

    fixed format issue with address verification instance URL

--- easypost/__init__.py
+++ easypost/__init__.py
@@ -496,7 +496,7 @@ class Resource(EasyPostObject):
         easypost_id = Requestor._utf8(easypost_id)
         base = self.class_url()
         param = quote_plus(easypost_id)
-        return "{}/{}".format(base, param)
+        return "%s/%s" % (base, param)
 
 
 # parent resource classes
