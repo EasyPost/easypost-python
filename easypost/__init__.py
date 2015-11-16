4f966864449f876725ce5ce7fe0e7e493c6e079c
sawyer <sawyerbateman@gmail.com>
2015-11-16 13:35:20 -0800

    Added a Tracker.all_updated method for retrieving a large number of Trackers by status or tracking_details updated datetime

--- easypost/__init__.py
+++ easypost/__init__.py
@@ -779,6 +779,14 @@ class Tracker(AllResource, CreateResource):
         response, api_key = requestor.request('post', url, params)
         return convert_to_easypost_object(response["trackers"], api_key)
 
+    @classmethod
+    def all_updated(cls, api_key=None, **params):
+        requestor = Requestor(api_key)
+        url = "%s/%s" % (cls.class_url(), "all_updated")
+        response, api_key = requestor.request('get', url, params)
+        print response
+        return convert_to_easypost_object(response["trackers"], api_key), response["has_more"]
+
 
 class Pickup(AllResource, CreateResource):
     def buy(self, **params):
