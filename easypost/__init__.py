299751a7951814c65e302783f2840566843465c7
sawyer <sawyerbateman@gmail.com>
2015-11-24 16:02:11 -0800

    Added Blob.retrieve for fetching urls for blobs stored by EasyPost.

--- easypost/__init__.py
+++ easypost/__init__.py
@@ -828,3 +828,17 @@ class CarrierAccount(AllResource, CreateResource, UpdateResource, DeleteResource
         requestor = Requestor(api_key)
         response, api_key = requestor.request('get', "/carrier_types")
         return convert_to_easypost_object(response, api_key)
+
+class Blob(AllResource, CreateResource):
+    @classmethod
+    def retrieve(cls, easypost_id, api_key=None, **params):
+        try:
+            easypost_id = easypost_id['id']
+        except (KeyError, TypeError):
+            pass
+
+        requestor = Requestor(api_key)
+        url = "%s/%s" % (cls.class_url(), easypost_id)
+        response, api_key = requestor.request('get', url)
+        return response["signed_url"]
+
