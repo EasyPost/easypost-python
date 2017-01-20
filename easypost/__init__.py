f7f6cf8fa03c4d49d18700f9e5e6de23268cb1e6
Jake Epstein <jake@easypost.com>
2017-01-19 16:41:44 -0800

    fixed ScanForm create and added tests

--- easypost/__init__.py
+++ easypost/__init__.py
@@ -659,7 +659,12 @@ class Address(AllResource, CreateResource):
 
 
 class ScanForm(AllResource, CreateResource):
-    pass
+    @classmethod
+    def create(cls, api_key=None, **params):
+        requestor = Requestor(api_key)
+        url = cls.class_url()
+        response, api_key = requestor.request('post', url, params)
+        return convert_to_easypost_object(response, api_key)
 
 
 class Insurance(AllResource, CreateResource):
---
+++ tests/scan_form.py
@@ -0,0 +1,87 @@
+# Unit tests related to 'ScanForms' (https://www.easypost.com/docs/api.html#manifesting-scan-form).
+
+import unittest
+import easypost
+from constants import API_KEY as api_key
+
+easypost.api_key = api_key
+
+
+class ScanFormTests(unittest.TestCase):
+
+    def test_scan_form_create_and_retrieve(self):
+        # prepare params for shipment
+        to_address = {
+            "name": "Sawyer Bateman",
+            "street1": "1A Larkspur Cres.",
+            "street2": "",
+            "city": "St. Albert",
+            "state": "AB",
+            "zip": "t8n2m4",
+            "country": "CA",
+            "phone": "780-283-9384"
+        }
+        from_address = {
+            "company": "EasyPost",
+            "street1": "118 2nd St",
+            "street2": "4th Fl",
+            "city": "San Francisco",
+            "state": "CA",
+            "zip": "94105",
+            "phone": "415-456-7890"
+        }
+        parcel = {
+            "length": 10.2,
+            "width": 7.8,
+            "height": 4.3,
+            "weight": 21.2
+        }
+        customs_item = {
+            "description": "EasyPost t-shirts",
+            "hs_tariff_number": 123456,
+            "origin_country": "US",
+            "quantity": 2,
+            "value": 96.27,
+            "weight": 21.1
+        }
+        customs_info = {
+            "customs_certify": 1,
+            "customs_signer": "Hector Hammerfall",
+            "contents_type": "gift",
+            "contents_explanation": "",
+            "eel_pfc": "NOEEI 30.37(a)",
+            "non_delivery_option": "return",
+            "restriction_type": "none",
+            "restriction_comments": "",
+            "customs_items": [customs_item]
+        }
+
+        # create shipment and buy
+        shipment = easypost.Shipment.create(
+            to_address=to_address,
+            from_address=from_address,
+            parcel=parcel,
+            customs_info=customs_info
+        )
+        shipment.buy(rate=shipment.lowest_rate(['USPS', 'ups'], 'priorityMAILInternational'), insurance=100.00)
+
+        # create scan form
+        scan_form = easypost.ScanForm.create(
+            shipments=[shipment]
+        )
+
+        # Assert values match
+        assert scan_form.id is not None
+        assert scan_form.tracking_codes[0] == shipment.tracking_code
+
+        # retrieve a copy of the scan form
+        scan_form2 = easypost.ScanForm.retrieve(scan_form.id)
+
+        # Assert values match
+        assert scan_form2.id == scan_form.id
+
+        # index scan_forms
+        scan_forms = easypost.ScanForm.all(page_size=2)
+
+        # Assert values match
+        assert scan_forms["scan_forms"][0].id == scan_form.id
