4142d852bfe8400117fe2a2fc90d8d0ef5dba84d
Justintime50 <jhammond@easypost.com>
2020-03-19 11:25:35 -0600

    Closes #95

--- easypost/version.py
+++ easypost/version.py
@@ -1,7 +1,8 @@
 import pkg_resources
 
-
-VERSION = pkg_resources.resource_stream('easypost', '../VERSION').read().decode('utf-8').strip()
+VERSION_FILE = pkg_resources.resource_stream('easypost', '../VERSION')
+VERSION = VERSION_FILE.read().decode('utf-8').strip()
+VERSION_FILE.close()
 
 if '-' in VERSION:
     VERSION_INFO = tuple([int(v) for v in VERSION.split('-')[0].split('.')] + VERSION.split('-')[1:])
