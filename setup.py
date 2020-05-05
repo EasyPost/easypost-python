8b14f670e03f0a8f5a3c69838dced094e4664b89
James Brown <jbrown@easypost.com>
2020-05-05 22:31:56 +0000

    stop doing tricks to populate version

--- setup.py
+++ setup.py
@@ -11,10 +11,6 @@ install_requires = [
 ]
 
 
-with open('VERSION', 'r') as f:
-    version = f.read().strip()
-
-
 if sys.version_info < (3, 0):
     long_description_open = io.open
 else:
@@ -26,13 +22,12 @@ with long_description_open('README.md', encoding='utf-8') as f:
 
 setup(
     name='easypost',
-    version=version,
+    version='4.0.1',
     description='EasyPost Shipping API Client Library for Python',
     author='EasyPost',
     author_email='support@easypost.com',
     url='https://easypost.com/',
     packages=['easypost'],
-    package_data={'easypost': ['../VERSION']},
     install_requires=install_requires,
     test_suite='test',
     long_description=long_description,
