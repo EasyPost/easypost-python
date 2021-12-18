a95ded003847c84d6a90609f50d0a12d72a29d6a
Nate Harris <nharris@easypost.com>
2021-12-17 23:18:47 -0500

    - Support for using GitHub Actions to trigger a release - User will only ever need to edit _info.py (if needed)
    Workflow Overview:
    - User creates a release on GitHub (no file edits required beforehand)
    - Making a release triggers the publish_to_pypi action:
      - Uses regex to replace VERSIONADDEDBYGITHUB with the tag name used when creating release (use X.Y.Z semver only; PyPi does not support non-digits and will fail)
      - Uses setup.py to create the Python package from release, pulling info from _info.py and some environmental variables passed into the GitHub Actions runner
      - Uses PYPI_USERNAME and PYPI_PASSWORD from GitHub Secrets to upload package to PyPi.org
    - Package's __author__ and __version__ are set in __init__.py, using details from _info.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import io
+import os
 import sys
 
 try:
@@ -6,10 +7,10 @@ try:
 except ImportError:
     from distutils.core import setup
 
-install_requires = [
-    "requests >= 2.4.3",
-    "six",
-]
+import easypost._info as package_info
+from easypost._version import VERSION
+
+from datetime import date
 
 
 if sys.version_info < (3, 0):
@@ -20,23 +21,28 @@ else:
 with long_description_open("README.md", encoding="utf-8") as f:
     long_description = f.read()
 
+COPYRIGHT = package_info.COPYRIGHT
+if not COPYRIGHT:
+    COPYRIGHT = "Copyright © {0} - {1}".format(date.today().year, package_info.AUTHOR)
 
 setup(
-    name="easypost",
-    version="6.0.0",
-    description="EasyPost Shipping API Client Library for Python",
-    author="EasyPost",
-    author_email="support@easypost.com",
-    url="https://easypost.com/",
-    packages=["easypost"],
-    install_requires=install_requires,
-    test_suite="test",
+    name=package_info.TITLE,
+    packages=[package_info.TITLE],
+    version=VERSION,
+    license=package_info.LICENSE,
+    description=package_info.DESCRIPTION,
     long_description=long_description,
     long_description_content_type="text/markdown",
+    author=package_info.AUTHOR,
+    author_email=package_info.AUTHOR_EMAIL,
+    url=package_info.URL,
+    download_url='https://github.com/{0}/archive/refs/tags/{1}.tar.gz'.format(os.environ["GITHUB_REPO"], {os.environ["TAG"]}),
+    install_requires=package_info.INSTALL_REQUIREMENTS,
+    test_suite="test",
     project_urls={
         "Docs": "https://www.easypost.com/docs/api",
-        "Tracker": "https://github.com/EasyPost/easypost-python/issues",
-        "Source": "https://github.com/EasyPost/easypost-python",
+        "Tracker": "https://github.com/{0}/issues".format(os.environ['GITHUB_REPO']),
+        "Source": "https://github.com/{0}".format(os.environ['GITHUB_REPO']),
     },
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, <4",
     classifiers=[
