f2253a41863646f01aceec71fdc12db221fc8c7c
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2022-02-01 13:32:08 -0700

    fix: lint

--- examples/unicode.py
+++ examples/unicode.py
@@ -3,7 +3,10 @@ import easypost
 easypost.api_key = "API_KEY"
 
 # unicode
+# TODO: Remove these `fmt` on/off lines once we drop support for Python 2
+# fmt: off
 state = u"DELEGACI\xf3N BENITO JU\xe1REZ"
+# fmt: on
 
 address = easypost.Address.create(state=state)
 
