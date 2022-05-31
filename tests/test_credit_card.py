1084f40a24e38c49b5da10eff2f761893bce3290
jchen293 <jchen@easypost.com>
2022-05-31 16:25:41 -0400

    change the fund amount from 100 to 20.

--- tests/test_credit_card.py
+++ tests/test_credit_card.py
@@ -7,7 +7,7 @@ import easypost
 @pytest.mark.skip("Skipping due to the lack of an available real credit card in tests")
 def test_credit_card_fund(prod_api_key):
     credit_card = easypost.CreditCard.fund(
-        amount="10",
+        amount="20",
         primary_or_secondary="primary",
     )
 
