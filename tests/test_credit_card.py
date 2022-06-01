7722ee945d545299bd70f409d528eb9eea1039bf
jchen293 <jchen293@buffalo.edu>
2022-06-01 14:05:51 -0400

    fix the credit card fund unit test parameters. (#198)

--- tests/test_credit_card.py
+++ tests/test_credit_card.py
@@ -7,8 +7,8 @@ import easypost
 @pytest.mark.skip("Skipping due to the lack of an available real credit card in tests")
 def test_credit_card_fund(prod_api_key):
     credit_card = easypost.CreditCard.fund(
+        amount="20",
         primary_or_secondary="primary",
-        amount="10",
     )
 
     assert credit_card
