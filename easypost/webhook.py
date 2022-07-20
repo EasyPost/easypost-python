f8db724f34192f0d0b54361cef15e57d4b98c28a
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2022-07-20 16:43:23 -0600

    feat: adds validate webhook function
    fix: breakup secrets logic for easier reading
    
    fix: event_body accepts bytes instead of a dictionary
    
    fix: remove dead code
    
    fix: do not validate a webhook with no signature
    
    fix: broken hmac matching

--- easypost/webhook.py
+++ easypost/webhook.py
@@ -1,3 +1,13 @@
+import hashlib
+import hmac
+import json
+import unicodedata
+from typing import (
+    Any,
+    Dict,
+)
+
+from easypost.error import Error
 from easypost.requestor import (
     RequestMethod,
     Requestor,
@@ -17,3 +27,35 @@ class Webhook(AllResource, CreateResource, DeleteResource):
         response, api_key = requestor.request(method=RequestMethod.PATCH, url=url, params=params)
         self.refresh_from(values=response, api_key=api_key)
         return self
+
+    @staticmethod
+    def validate_webhook(event_body: bytes, headers: Dict[str, Any], webhook_secret: str) -> Dict[str, Any]:
+        """Validate a webhook by comparing the HMAC signature header sent from EasyPost to your shared secret.
+        If the signatures do not match, an error will be raised signifying the webhook either did not originate
+        from EasyPost or the secrets do not match. If the signatures do match, the `event_body` will be returned
+        as JSON.
+        """
+        easypost_hmac_signature = headers.get("X-Hmac-Signature")
+
+        if easypost_hmac_signature:
+            normalized_secret = unicodedata.normalize("NFKD", webhook_secret)
+            encoded_secret = bytes(normalized_secret, "utf8")
+
+            expected_signature = hmac.new(
+                key=encoded_secret,
+                msg=event_body,
+                digestmod=hashlib.sha256,
+            )
+
+            digest = "hmac-sha256-hex=" + expected_signature.hexdigest()
+
+            if hmac.compare_digest(digest, easypost_hmac_signature):
+                webhook_body = json.loads(event_body)
+            else:
+                raise Error(
+                    message="Webhook received did not originate from EasyPost or had a webhook secret mismatch."
+                )
+        else:
+            raise Error(message="Webhook received does not contain an HMAC signature.")
+
+        return webhook_body
