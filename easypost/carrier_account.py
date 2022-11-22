f83ac534f50c737907c90e0948e60d7c1bb5483a
nharris <nharris@easypost.com>
2022-11-22 14:55:05 -0700

    - Overload existing `create` function in carrier_account to dynamically adjust URL based on carrier account type - Update unit test, cassette

--- easypost/carrier_account.py
+++ easypost/carrier_account.py
@@ -16,6 +16,19 @@ from easypost.resource import (
 )
 
 
+def _select_carrier_account_creation_endpoint(carrier_account_type: str) -> str:
+    """Determines which API endpoint to use for the creation call."""
+    carriers_with_custom_workflows = [
+        "FedExAccount",
+        "UpsAccount",
+    ]
+
+    if carrier_account_type in carriers_with_custom_workflows:
+        return f"/carrier_accounts/register"
+
+    return "/carrier_accounts"
+
+
 class CarrierAccount(AllResource, CreateResource, UpdateResource, DeleteResource):
     @classmethod
     def types(cls, api_key: Optional[str] = None) -> List[str]:
@@ -24,11 +37,16 @@ class CarrierAccount(AllResource, CreateResource, UpdateResource, DeleteResource
         response, api_key = requestor.request(method=RequestMethod.GET, url="/carrier_types")
         return convert_to_easypost_object(response=response, api_key=api_key)
 
+    # Overrides CreateResource.create
     @classmethod
-    def register(cls, api_key: Optional[str] = None, **params) -> "CarrierAccount":
+    def create(cls, api_key: Optional[str] = None, **params) -> "CarrierAccount":
         """Creates a Carrier Account that requires custom registration (eg: FedEx & UPS)."""
         requestor = Requestor(local_api_key=api_key)
-        url = f"{cls.class_url()}/register"
+
+        if params.get("type") is None:
+            raise ValueError("Missing required parameter 'type'")
+
+        url = _select_carrier_account_creation_endpoint(params.get("type"))
         wrapped_params = {cls.snakecase_name(): params}
         response, api_key = requestor.request(method=RequestMethod.POST, url=url, params=wrapped_params)
         return convert_to_easypost_object(response=response, api_key=api_key)
