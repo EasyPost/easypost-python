fc337e4e244b762c7d5e5ef14ca60f5bbc655ce8
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2022-12-14 11:16:47 -0700

    fix: move functions to beta module

--- easypost/beta/referral.py
+++ easypost/beta/referral.py
@@ -110,6 +110,67 @@ class Referral:
         return convert_to_easypost_object(response)
 
     @staticmethod
+    def add_payment_method(
+        stripe_customer_id: str,
+        payment_method_reference: str,
+        priority: Optional[str] = "primary",
+    ) -> Dict[str, Any]:
+        """Add a Stripe payment method to your EasyPost account.
+
+        This endpoint uses a user's personal Stripe account. The `stripe_customer_id`
+        and `payment_method_reference` IDs both come from Stripe. By adding these to
+        EasyPost, we will associate your Stripe payment method with either your primary
+        or secondary EasyPost payment method.
+        """
+        requestor = Requestor()
+        wrapped_params = {
+            "payment_method": {
+                "stripe_customer_id": stripe_customer_id,
+                "payment_method_reference": payment_method_reference,
+                "priority": priority,
+            }
+        }
+
+        response, api_key = requestor.request(
+            method=RequestMethod.POST,
+            url="/referral_customers/payment_method",
+            params=wrapped_params,
+            beta=True,
+        )
+
+        return convert_to_easypost_object(response=response, api_key=api_key)
+
+    @staticmethod
+    def refund_by_amount(refund_amount: str) -> Dict[str, Any]:
+        """Refund a ReferralCustomer wallet by specifying an amount."""
+        requestor = Requestor()
+        wrapped_params = {"refund_amount": refund_amount}
+
+        response, api_key = requestor.request(
+            method=RequestMethod.POST,
+            url="/referral_customers/refunds",
+            params=wrapped_params,
+            beta=True,
+        )
+
+        return convert_to_easypost_object(response=response, api_key=api_key)
+
+    @staticmethod
+    def refund_by_payment_log(payment_log_id: str) -> Dict[str, Any]:
+        """Refund a ReferralCustomer wallet by specifying a payment log ID to completely refund."""
+        requestor = Requestor()
+        wrapped_params = {"payment_log_id": payment_log_id}
+
+        response, api_key = requestor.request(
+            method=RequestMethod.POST,
+            url="/referral_customers/refunds",
+            params=wrapped_params,
+            beta=True,
+        )
+
+        return convert_to_easypost_object(response=response, api_key=api_key)
+
+    @staticmethod
     def _retrieve_easypost_stripe_api_key() -> str:
         """Retrieve EasyPost's Stripe public API key."""
         requestor = Requestor()
--- tests/test_beta_referral.py
+++ tests/test_beta_referral.py
@@ -75,7 +75,7 @@ def test_beta_referral_add_credit_card(partner_prod_api_key, credit_card_details
 
 @patch("easypost.beta.referral.Referral._retrieve_easypost_stripe_api_key")
 @patch("easypost.beta.referral.Referral._create_stripe_token", side_effect=Exception())
-def test_referral_add_credit_card_error(mock_stripe_token, mock_easypost_key, credit_card_details):
+def test_beta_referral_add_credit_card_error(mock_stripe_token, mock_easypost_key, credit_card_details):
     """This test requires a partner user's production API key via PARTNER_USER_PROD_API_KEY
     as well as one of that user's referral's production API keys via REFERRAL_USER_PROD_API_KEY.
     """
@@ -89,3 +89,43 @@ def test_referral_add_credit_card_error(mock_stripe_token, mock_easypost_key, cr
         )
 
     assert str(error.value) == "Could not send card details to Stripe, please try again later"
+
+
+@pytest.mark.vcr()
+def test_beta_referral_add_payment_method(referral_customer_prod_api_key):
+    """This test requires a referral customer user's production API key via REFERRAL_CUSTOMER_PROD_API_KEY.
+
+    We expect this test to fail because we don't have valid Stripe details to use. Assert the correct error.
+    """
+    with pytest.raises(Exception) as error:
+        easypost.beta.Referral.add_payment_method(
+            stripe_customer_id="cus_123",
+            payment_method_reference="ba_123",
+            priority="primary",
+        )
+
+    assert str(error.value) == "Invalid Payment Gateway Reference."
+
+
+@pytest.mark.vcr()
+def test_beta_referral_refund_by_amount(referral_customer_prod_api_key):
+    """This test requires a referral customer user's production API key via REFERRAL_CUSTOMER_PROD_API_KEY.
+
+    We expect this test to fail because we don't have valid billing details to use. Assert the correct error.
+    """
+    with pytest.raises(Exception) as error:
+        easypost.beta.Referral.refund_by_amount(refund_amount="$20.00")
+
+    assert str(error.value) == "Refund amount doesn't match. Please escalate this to finance, this is unexpected."
+
+
+@pytest.mark.vcr()
+def test_beta_referral_refund_by_payment_log(referral_customer_prod_api_key):
+    """This test requires a referral customer user's production API key via REFERRAL_CUSTOMER_PROD_API_KEY.
+
+    We expect this test to fail because we don't have valid billing details to use. Assert the correct error.
+    """
+    with pytest.raises(Exception) as error:
+        easypost.beta.Referral.refund_by_payment_log(payment_log_id="paylog_123")
+
+    assert str(error.value) == "We could not find a transaction with that id."
