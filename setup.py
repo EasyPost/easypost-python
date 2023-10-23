6d6065cea5b73550eebe0c69be22dd8d75b256c8
Nate Harris <nwithan8@users.noreply.github.com>
2023-10-23 15:04:08 -0600

    - Update changelog, version number (#301)

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="easypost",
-    version="7.13.1",
+    version="7.14.0",
     description="EasyPost Shipping API Client Library for Python",
     author="EasyPost",
     author_email="support@easypost.com",
