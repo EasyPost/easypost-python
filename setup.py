684e38038be8b9355d2a78259036efbd39e68aef
nwithan8 <nwithan8@users.noreply.github.com>
2023-10-30 10:13:52 -0600

    - Prep for v7.14.1 release

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="easypost",
-    version="7.14.0",
+    version="7.14.1",
     description="EasyPost Shipping API Client Library for Python",
     author="EasyPost",
     author_email="support@easypost.com",
